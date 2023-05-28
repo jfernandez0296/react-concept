789736e3a290baef0f98c0566368482f91377d5e
Jaime Fernández Chávez <jfernandezc@uni.pe>
2023-05-28 12:07:16 -0500

    Nuevos cambios

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import logo from './logo.svg';
 
 import { CustomRouter } from './router';
+import Loader from './components/Loader/Loader';
+import { useState } from 'react';
 
 import './App.scss';
 
 function App() {
+
+  const [loading, setLoading] = useState(false);
+
+  const cambiarEstado=()=>{
+
+    setLoading(true);
+    setTimeout(()=>{
+      setLoading(false);
+    },5000);
+  }
+
+  if(loading)
+  {
+
+    
+  }
   return (
     <div className="App">
 
---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Loader.scss'; // Asegúrate de que este archivo CSS exista en la misma carpeta
+
+const Loader = () => {
+  return (
+    <div className="loader-container">
+      <div className="loader"></div>
+    </div>
+  );
+};
+
+export default Loader;
